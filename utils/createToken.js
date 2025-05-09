84e98092a63e684293a554792e18fe9cc2630949
Babaefff <ilkinbabayevv777@gmail.com>
2025-05-10 01:59:33 +0400

    token

--- utils/createToken.js
+++ utils/createToken.js
@@ -4,11 +4,11 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-
+  console.log("creating", token);
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
